89cb52bd03f5b2c8ad4b868326cd5bd7e78c8228
Jiten Kumar Mohanty <jitenmohantyaz@gmail.com>
2023-11-11 19:38:11 +0530

    Client side work done finally

--- FormValidation/src/Components/Registation.jsx
+++ FormValidation/src/Components/Registation.jsx
@@ -1,60 +1,74 @@
-import React, { useState } from 'react';
-import { useNavigate } from 'react-router-dom';
-import { motion } from 'framer-motion';
-import { toast } from 'react-toastify';
+import { useState } from "react";
+import { motion } from "framer-motion";
+import { toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+import { useNavigate } from "react-router-dom";
 
 const Registration = () => {
   const [selectedHobbies, setSelectedHobbies] = useState([]);
   const [userData, setUserData] = useState({
-    name: '',
-    dob: '',
-    gender: '',
+    name: "",
+    dob: "",
+    gender: "",
     hobbies: [],
-    state: '',
+    state: "",
     file: null,
-    address: '',
+    address: "",
   });
 
   const navigate = useNavigate();
 
   const handleFile = (e) => {
-    const allowedFileTypes = ['docx'];
+    const allowedFileTypes = ["docx"];
     const fileInput = e.target;
     const selectedFile = fileInput.files[0];
 
     if (selectedFile) {
-      const selectedFileType = selectedFile.name.split('.').pop().toLowerCase();
+      const selectedFileType = selectedFile.name.split(".").pop().toLowerCase();
 
-      console.log('Selected file type:', selectedFileType);
+      console.log("Selected file type:", selectedFileType);
 
       if (allowedFileTypes.includes(selectedFileType)) {
-        console.log('File type is valid:', selectedFileType);
+        console.log("File type is valid:", selectedFileType);
         setUserData((prevUserData) => ({
           ...prevUserData,
           file: selectedFile,
         }));
       } else {
-        console.error('Invalid file type!');
-        toast.error('Invalid file type!');
-        fileInput.value = '';
+        console.error("Invalid file type!");
+        toast.error("Invalid file type!");
+        fileInput.value = "";
       }
     }
   };
 
   const handleHobbyChange = (e) => {
     const selectedHobby = e.target.value;
-    const updatedHobbies = [...selectedHobbies];
 
-    if (selectedHobbies.includes(selectedHobby)) {
-      // Remove hobby if already selected
-      const index = updatedHobbies.indexOf(selectedHobby);
-      updatedHobbies.splice(index, 1);
-    } else {
-      // Add hobby if not selected
-      updatedHobbies.push(selectedHobby);
-    }
+    setUserData((prevUserData) => {
+      const updatedHobbies = [...prevUserData.hobbies];
 
-    setSelectedHobbies(updatedHobbies);
+      if (updatedHobbies.includes(selectedHobby)) {
+        // Remove hobby if already selected
+        const index = updatedHobbies.indexOf(selectedHobby);
+        updatedHobbies.splice(index, 1);
+      } else {
+        // Add hobby if not selected
+        updatedHobbies.push(selectedHobby);
+      }
+
+      // Ensure at least two hobbies are selected
+      if (updatedHobbies.length < 2) {
+        // Optionally, you can display a message or perform an action here
+        console.log("Select at least two hobbies");
+      }
+      setSelectedHobbies(updatedHobbies)
+      return {
+        ...prevUserData,
+        hobbies: updatedHobbies,
+       
+      };
+    });
   };
 
   const handleChange = (e) => {
@@ -86,25 +100,230 @@ const Registration = () => {
         transition={{ duration: 0.8, ease: "easeInOut" }}
         className="w-full md:w-1/2"
       >
-        {/* Your existing image or content */}
+        <img
+          className="w-full h-full rounded-lg hidden md:block"
+          src="https://media.istockphoto.com/id/1281150061/vector/register-account-submit-access-login-password-username-internet-online-website-concept.jpg?s=612x612&w=0&k=20&c=9HWSuA9IaU4o-CK6fALBS5eaO1ubnsM08EOYwgbwGBo="
+          alt=""
+        />
       </motion.div>
 
       <div className="container mx-auto p-8 bg-orange-200 rounded-lg shadow-md w-full md:w-1/2">
         <div className="flex justify-between">
           <h1 className="text-3xl font-bold mb-6">Registration Form</h1>
-          <button onClick={() => navigate('/user-data')} className="bg-black text-white rounded-md font-bold mb-6 p-2">
+          <button
+            onClick={() => navigate("/user-data")}
+            className="bg-black text-white rounded-md font-bold mb-6 p-2"
+          >
             Display All User
           </button>
         </div>
-        <form action="/submit" method="post" encType="multipart/form-data" onSubmit={handleSubmit}>
-          {/* ... (unchanged code) */}
+        <form
+          action="/submit"
+          method="post"
+          encType="multipart/form-data"
+          onSubmit={handleSubmit}
+        >
+          <div className="flex flex-wrap -mx-4 mb-4 font-semibold">
+            <div className="w-full md:w-1/2 px-4 mb-4 md:mb-0">
+              <label
+                htmlFor="name"
+                className="block text-sm font-medium text-gray-600"
+              >
+                Name:
+              </label>
+              <input
+                type="text"
+                id="name"
+                name="name"
+                className="mt-1 p-2 border rounded-md w-full"
+                value={userData.name}
+                onChange={handleChange}
+              />
+            </div>
+            <div className="w-full md:w-1/2 px-4">
+              <label
+                htmlFor="dob"
+                className="block text-sm font-medium text-gray-600"
+              >
+                Date of Birth:
+              </label>
+              <input
+                type="date"
+                id="dob"
+                name="dob"
+                className="mt-1 p-2 border rounded-md w-full"
+                value={userData.dob}
+                onChange={handleChange}
+              />
+            </div>
+          </div>
 
-          {/* Submit Button */}
+          <div className="mb-4 flex">
+            <label className="block text-sm font-medium text-gray-600">
+              Gender:
+            </label>
+            <div className="mx-4">
+              <input
+                type="radio"
+                id="male"
+                name="gender"
+                value="male"
+                checked={userData.gender === "male"}
+                onChange={handleChange}
+              />
+              <label htmlFor="male" className="ml-1">
+                Male
+              </label>
+            </div>
+            <div className="mx-1">
+              <input
+                type="radio"
+                id="female"
+                name="gender"
+                value="female"
+                checked={userData.gender === "female"}
+                onChange={handleChange}
+              />
+              <label htmlFor="female" className="ml-1">
+                Female
+              </label>
+            </div>
+            <div className="mx-1">
+              <input
+                type="radio"
+                id="other"
+                name="gender"
+                value="other"
+                checked={userData.gender === "other"}
+                onChange={handleChange}
+              />
+              <label htmlFor="other" className="ml-1">
+                Other
+              </label>
+            </div>
+          </div>
+
+          <div className="mb-4 flex">
+            <label className="block text-sm font-medium text-gray-600">
+              Hobbies:
+            </label>
+            <div className="mx-1 ml-2">
+              <input
+                type="checkbox"
+                id="Reading"
+                name="hobbies"
+                value="Reading"
+                onChange={handleHobbyChange}
+              />
+              <label htmlFor="hobby1" className="ml-1">
+                Reading
+              </label>
+            </div>
+            <div className="mx-1">
+              <input
+                type="checkbox"
+                id="Dancing"
+                name="hobbies"
+                value="Dancing"
+                onChange={handleHobbyChange}
+              />
+              <label htmlFor="hobby2" className="ml-1">
+                Dancing
+              </label>
+            </div>
+            <div className="mx-1">
+              <input
+                type="checkbox"
+                id="Skating"
+                name="hobbies"
+                value="Skating"
+                onChange={handleHobbyChange}
+              />
+              <label htmlFor="hobby3" className="ml-1">
+                Skating
+              </label>
+            </div>
+            <div className="mx-1">
+              <input
+                type="checkbox"
+                id="Cycling"
+                name="hobbies"
+                value="Cycling"
+                onChange={handleHobbyChange}
+              />
+              <label htmlFor="hobby4" className="ml-1">
+                Cycling
+              </label>
+            </div>
+          </div>
+
+          <div className="mb-4">
+            <label
+              htmlFor="state"
+              className="block text-sm font-medium text-gray-600 "
+            >
+              State:
+            </label>
+            <select
+              id="state"
+              name="state"
+              className="mt-1 p-2 border rounded-md w-full"
+              onChange={handleChange}
+            >
+              <option value="Odisha">Odisha</option>
+              <option value="Panjab">Panjab</option>
+              <option value="Hariyana">Hariyana</option>
+              <option value="Moharastra">Moharastra</option>
+              {/* Add more options as needed */}
+            </select>
+          </div>
+
+          <div className="mb-4">
+            <label
+              htmlFor="pdf"
+              className="block text-sm font-medium text-gray-600"
+            >
+              Upload PDF:
+            </label>
+            <input
+              type="file"
+              id="pdf"
+              name="file"
+              className="mt-1 p-2 border rounded-md w-full bg-white"
+              onChange={handleFile}
+              required
+            />
+            <p className="text-xs text-gray-500 mt-1">
+              Only .docx files are allowed.
+            </p>
+          </div>
+
+          <div className="mb-4">
+            <label
+              htmlFor="address"
+              className="block text-sm font-medium text-gray-600"
+            >
+              Address:
+            </label>
+            <textarea
+              id="address"
+              name="address"
+              rows="4"
+              className="mt-1 p-2 border rounded-md w-full"
+              value={userData.address}
+              onChange={handleChange}
+              required
+            />
+          </div>
+
           <div className="flex justify-end">
             <button type="reset" className="bg-white text-black p-2 rounded-md">
               Reset All
             </button>
-            <button type="submit" className="bg-yellow-400 text-black p-2 rounded-md ml-2">
+            <button
+              type="submit"
+              className="bg-yellow-400 text-black p-2 rounded-md ml-2"
+            >
               Submit Form
             </button>
           </div>
